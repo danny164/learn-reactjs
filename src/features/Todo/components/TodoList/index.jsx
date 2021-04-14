ef26c500f8d77d250dff49f983961c3e29eff727
DANNY <qnv164@gmail.com>
2021-04-14 17:40:52 +0700

    Ok, perfect!

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,22 +1,50 @@
-// TODO 3.
-// **** Type: rfcp
+//
+// Type: rfcp
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
 
 // ! TodoList: Child Component
 function TodoList(props) {
-    // TODO 6.
-    const { todoList } = props;
+    const { todoList, onTodoClick } = props;
+
+    // TODO 1.4
+    const handleTodoClick = (todo, index) => {
+        // ? check props, if not data, will do nothing
+        if (!onTodoClick) return;
+        console.log(onTodoClick);
+
+        // ? else, send to parent 2 params (todo, index) via props
+        onTodoClick(todo, index);
+    };
+
+    // !
     return (
-        // TODO 7.
-        <ul>
+        //
+        <ul className="todo-list">
             {
-                // * traverse the array todoList via . map
-                // * for each todo, create a JSX use the parathesis ()
-                // ****
-                todoList.map((todo) => (
-                    <li key={todo.id}>{todo.title}</li>
+                //  traverse the array todoList via . map
+                //  for each todo, create a JSX use the parathesis ()
+                //  TODO 1.1 todoLis.map((todo)) => {}
+                //  * <li key={todo.id}>{todo.title}</li>
+                //  TODO 1.2 npm i --save classnames
+                //  * pass inside classnames will be an object
+                //  TODO 1.3 for each click, help me call handleTodoClick function
+                //  * with 2 params todo and index
+
+                todoList.map((todo, index) => (
+                    <li
+                        key={todo.id}
+                        className={classnames({
+                            'todo-item': true,
+                            completed: todo.status === 'completed',
+                        })}
+                        onClick={() => handleTodoClick(todo, index)}
+                    >
+                        {todo.title}
+                    </li>
                 ))
             }
         </ul>
@@ -24,15 +52,16 @@ function TodoList(props) {
 }
 
 TodoList.propTypes = {
-    // TODO 4.
-    // **** Declare an array called todoList, and no required the value
+    // Declare an array called todoList, and no required the value
     todoList: PropTypes.array, // ? isRequired
+
+    onTodoClick: PropTypes.func,
 };
 
 TodoList.defaultProps = {
-    // TODO 5.
-    // **** So, need a default value if the data is empty
+    // So, need a default value if the data is empty
     todoList: [], // ? if not, give an empty array
+    onTodoClick: null,
 };
 
 export default TodoList;
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,42 +1,103 @@
-// Todo 1. Type: rfcp
+// Type: rfcp
 
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
-// ! TodoFeature: Parent Container contains the value (a todoList Array)
-// * but doesn't know how to render
-// * so it needs a component TodoList to help
-// *****
+//  ! TodoFeature: Parent Container contains the value (a todoList Array)
+//  but doesn't know how to render
+//  so it needs a component TodoList to help
+//
 function TodoFeature(props) {
-    //  Todo 2. create...
-    //  **** an array contains objects
-    const todoList = [
+    //
+    //  Creat an array contains objects
+    const initTodoList = [
         {
             id: 1, // ? used to key value
             title: 'Eat',
+            status: 'new',
         },
         {
             id: 2,
             title: 'Sleep',
+            status: 'completed',
         },
         {
             id: 3,
             title: 'Code',
+            status: 'new',
         },
     ];
-    // !
-    // TODO 8. Render Child Component with Parent's Data via props `todoList`
-    // **** Inside curly braces {} the input value is an array also named { todoList }
+
+    // TODO 1. Create a useState with initialize value
+    const [todoList, setTodoList] = useState(initTodoList);
+
+    //////////////////////////////////////
+    // ! To update a state for a item we need to know the index item in array
+    // * By that way we can change it's status
+    // TODO 2.1 This function help us to change status
+    //////////////////////////////////////
+    // ! 3 Ways to Copy Objects in JavaScript
+    // * Use the spread (...) syntax
+    // - Use the Object.assign() method
+    // - Use the JSON.stringify() and JSON.parse() methods
+    // https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
+
+    const handleTodoClick = (todo, index) => {
+        //////////////////////////////////////
+        console.log(todo, index);
+        //////////////////////////////////////
+        // TODO 2.2
+        // * clone current array to the new one
+        const newTodoList = [...todoList];
+        //////////////////////////////////////
+        // TODO 2.3
+        // * toggle state
+        const newTodo = {
+            // using shallow copy to take all properties at the index
+            ...newTodoList[index],
+            // then change only status
+            status: newTodoList[index].status === 'new' ? 'completed' : 'new',
+        };
+        //////////////////////////////////////
+        // TODO 2.4
+        newTodoList[index] = newTodo;
+
+        //////////////////////////////////////
+        // ! The way 2: we can combine 2.3 & 2.4 as follows
+        // Mình muốn cập nhật vô cái vị trí index đó
+        // * newTodoList[index] = {
+        //   Với những giá trị hiện tại của nó
+        //   * ...newTodoList[index],
+        //   Đồng thời mình cũng thy đổi cái status của nó
+        //   * status: newTodoList[index].status === 'new' ? 'completed' : 'new',
+        // * };
+        //////////////////////////////////////
+
+        // TODO 2.5
+        // * update todo list
+        //////////////////////////////////////
+        setTodoList(newTodoList);
+        //////////////////////////////////////
+    };
+
+    //////////////////////////////////////
+    // Render Child Component with Parent's Data via props `todoList`
+    // Inside curly braces {} the input value is an array also named { todoList }
+    //////////////////////////////////////
+    // TODO 2: For each click, we need to report the event from child to parent
+    // ? Where to report? a `handleTodoClick` function via `onTodoClick` props*/
+    // * And the function will be callback after each click
+    // * Add `onTodoClick={handleTodoClick}` and go to 2.1
+    //////////////////////////////////////
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList} />
+            <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
         </div>
     );
 }
 
-// TODO 9.
-// **** Declare TodoFeature in App to show UI
+// Declare TodoFeature in App to show UI
 
 TodoFeature.propTypes = {};
 
